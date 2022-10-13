928f4330cf75c2c50c4e1353b0b5529730dccb32
Phil Schanely <philschanely@gmail.com>
2022-10-13 18:53:32 -0400

    feat: set up Next for Shiftio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+const ShiftioApp = ({ Component, pageProps }: AppProps) => (
+      <h1>Shift.io</h1>
+);
+
+export default ShiftioApp;
